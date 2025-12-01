775ea52c7b9e25434b401d6e7bd2d96d831afbbf
Teraz6 <retenk@gmail.com>
2025-12-01 17:51:07 +0200

    Add GetByID

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,5 +1,6 @@
 const { db } = require('../db')
 const Utilities = require('./Utilities')
+const UUID = require ('uuid')
 
 exports.getAll = 
 async (req, res) => {
@@ -13,16 +14,18 @@ exports.getByID =
 async (req, res) => {
     const product = await getProduct(req, res);
     if (!product) {return res.status(404).send({error: 'Product not found'})}
+    return res.status(200).send(product)
 }
 
-const getProducts = 
+const getProduct = 
 async (req, res) => {
     const idNumber = req.params.ProductID
-    if (isNaN(idNumber)){
-        res.status(400).send({error:`Entered id is not valid ${idNumber}`})
-        return null;
-    }
-    const product = await db.products.findByPk(idNumber)
+    console.log(idNumber)
+    // if (isNaN(idNumber)){
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }
+    const product = await db.Products.findByPk(idNumber)
     if(!product) {
         res.status(404).send({Error: `Product with this id was not found ${idNumber}`})
         return null;
