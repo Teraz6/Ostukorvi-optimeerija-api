5b6f4957626ee5afc46671174975b6f400dd7dcb
Teraz6 <retenk@gmail.com>
2025-12-02 12:37:48 +0200

    Update ProductsController.js

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -10,25 +10,3 @@ async (req, res) => {
     .send(products.map(({ProductID,Name}) => {return{ProductID,Name}}))
 }
 
-exports.getByID = 
-async (req, res) => {
-    const product = await getProduct(req, res);
-    if (!product) {return res.status(404).send({error: 'Product not found'})}
-    return res.status(200).send(product)
-}
-
-const getProduct = 
-async (req, res) => {
-    const idNumber = req.params.ProductID
-    console.log(idNumber)
-    // if (isNaN(idNumber)){
-    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
-    //     return null;
-    // }
-    const product = await db.Products.findByPk(idNumber)
-    if(!product) {
-        res.status(404).send({Error: `Product with this id was not found ${idNumber}`})
-        return null;
-    }
-    return product
-}
