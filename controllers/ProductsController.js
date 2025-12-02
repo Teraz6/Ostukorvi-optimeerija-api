54a4bee578bd77216cf0f8ca1da7757b445108d5
Teraz6 <retenk@gmail.com>
2025-12-02 12:38:38 +0200

    Update ProductsController.js

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -2,11 +2,3 @@ const { db } = require('../db')
 const Utilities = require('./Utilities')
 const UUID = require ('uuid')
 
-exports.getAll = 
-async (req, res) => {
-    const products = await db.Products.findAll();
-    console.log("getAll: " + products)
-    res.status(200)
-    .send(products.map(({ProductID,Name}) => {return{ProductID,Name}}))
-}
-
