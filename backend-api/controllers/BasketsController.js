b37eaf722d1119b5a1388320cb649e9fc7a3ad3c
Teraz6 <retenk@gmail.com>
2025-12-09 13:12:09 +0200

    Add getBasket method and add routes that fixes getById function

--- backend-api/controllers/BasketsController.js
+++ backend-api/controllers/BasketsController.js
@@ -2,6 +2,19 @@ const { db } = require('../db')
 const Utilities = require('./Utilities')
 const UUID = require ('uuid')
 
+const getBasket = 
+async (req, res) => {
+    const idNumber = req.params.BasketID
+    console.log(idNumber)
+
+    const basket = await db.Basket.findByPk(idNumber)
+    if(!basket) {
+        res.status(404).send({Error: `Basket with this id was not found ${idNumber}`})
+        return null;
+    }
+    return basket
+}
+
 exports.create = 
 async (req, res) => {
     if (
@@ -18,7 +31,7 @@ async (req, res) => {
     }
 
     const createdBasket = await db.Basket.create(newBasket)
-    return res.location(`${Utilities.getBaseURL(req)}/baskets/${createdBasket.BasketID}`)
+    return res.location(`${Utilities.getBaseURL(req)}/basket/${createdBasket.BasketID}`)
     .sendStatus(201);
 }
 
--- backend-api/routes/basketRoutes.js
+++ backend-api/routes/basketRoutes.js
@@ -2,5 +2,8 @@ const BasketController = require("../controllers/BasketsController")
 
 module.exports = (app) => {
     app.route("/baskets")
+    .get(BasketController.getAll)
     .post(BasketController.create)
+    app.route("/baskets/:BasketID")
+    .get(BasketController.getById)
 }
