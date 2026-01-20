9867d3e35e1a31b7a9697f200d60695df1dd5aa0
Teraz6 <retenk@gmail.com>
2026-01-20 17:01:53 +0200

    Add password confirmation to sign up form

--- frontend-site/ostukorvi-optimeerija/src/components/SignUpForm.vue
+++ frontend-site/ostukorvi-optimeerija/src/components/SignUpForm.vue
@@ -1,22 +1,45 @@
 <script>
     export default {
+        computed: {
+            passwordError() {
+                if (this.NewProfile.PlainPassword && this.passwordConfirm) {
+                    return this.NewProfile.PlainPassword !== this.passwordConfirm;
+                }
+                return false;
+            }
+        },
         methods: {
             async signUp() {
-                console.log("button is clicked")
-                    await fetch(`http://localhost:8080/profiles`, {
+
+                if (this.passwordError) {
+                    alert("Please fix the errors");
+                    return;
+                }
+
+                const response = await fetch(`http://localhost:8080/profiles`, {
                     method: 'POST',
                     headers: {'Content-Type' : 'application/json'},
                     body: JSON.stringify(this.NewProfile),
                 })
+
+                if (response.ok) {
+                    alert("Profile registered")
+                    this.$router.push('/login')
+                } else {
+                    alert("Error creating a profile")
+                }
             }
         },
         name: "SignUpForm",
         data() {
-            return {NewProfile: {
-                Name: "Your name",
-                Email: "your@email.com",
-                PlainPassword: ""
-            }}
+            return {
+                NewProfile: {
+                    Name: "",
+                    Email: "",
+                    PlainPassword: "",
+                },
+                passwordConfirm: ""
+            }
         }
     };
 </script>
@@ -27,20 +50,21 @@
         <form @submit.prevent="signUp">
             <div class="form-group">
                 <label>Name:</label>
-                <input v-model="NewProfile.Name" type="text" required/>
+                <input v-model="NewProfile.Name" placeholder="Your Name" type="text" required/>
             </div>
             <div class="form-group">
                 <label>Email:</label>
-                <input v-model="NewProfile.Email" type="text" required/>
+                <input v-model="NewProfile.Email" placeholder="Your@email.here" type="text" required/>
             </div>
             <div class="form-group">
                 <label>Password:</label>
                 <input v-model="NewProfile.PlainPassword" type="password" required/>
             </div>
-            <!-- <div class="form-group">
-                <label>Confirm password:</label>
-                <input type="password" required/>
-            </div> -->
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input v-model="passwordConfirm" type="password" />
+                <span v-if="passwordError" style="color:red;">Passwords don't match</span>
+            </div>
             <div class="btn-group">
                 <button type="submit" class="save-btn">Register</button>
             </div>
